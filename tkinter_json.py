c67a3ab1666c5a08e52ae80af418a02a8af5c75b
Shimon Sant <122385952+shimonsant2@users.noreply.github.com>
2023-08-02 16:14:53 +0300

    changing parameters

--- tkinter_json.py
+++ tkinter_json.py
@@ -7,9 +7,9 @@ text = tk.Text(app)
 
 text.pack()
 
-json_val = json.loads('{"a": 5, "b": 7}')
+json_val = json.loads('{"a": 8, "b": 9}')
 
 for k in json_val:
-    text.insert(tk.END, '{} = {}\n'.format(k,json_val[k]))
-text.config(state = tk.DISABLED)
+    text.insert(tk.END, '{} = {}\n'.format(k, json_val[k]))
+text.config(state=tk.DISABLED)
 app.mainloop()
